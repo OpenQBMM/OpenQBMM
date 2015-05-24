ed85c9f9ff3abece4ae2de07a69e288d88918b89
Alberto Passalacqua <apcfd@outlook.com>
2015-05-24 21:56:25 +0200

    DEV: Implemented templated class to store a moment of a      distribution function with arbitrary dimensionality.
         The moment is templated with respect to the type of field
         (volField, surfaceField, ...) of the moment itself, and
         to the type of the quadrature node used in the quadrature
         approximation.
    
         The order of the moment is established summing its indeces,
         which are read from a labelList when the object is
         constructed.For example, to define the moment m131, the
         labelList (1 3 1) will be used.
    
         In case the quadrature abscissa has multiple components,
         as for velocity moments, and quadrature abscissae
         represented by vectorField, the moment is calculated using
         each component of the abscissa field elevated to the
         corresponding exponent extracted from the order of the
         moment.
         For example, if the moment m131 is considered, it is
         assumed that the abscissa field has three components,
         whose exponents will be 1, 3 and 1, respectively.

---
+++ src/quadratureMethods/moments/moment.C
@@ -0,0 +1,183 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2012 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+2015-03-09 Alberto Passalacqua: Templated class on the type of field used to
+                                store the moment and on the type of quadrature
+                                node.
+2015-05-23 Alberto Passalacqua: Added IOobject::groupname for improved naming
+                                of files associated to moments.
+2015-05-24 Alberto Passalacqua: Generalized moment update function to deal with
+                                standard and extended nodes.
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "moment.H"
+
+// * * * * * * * * * * * * * Static Member Functions * * * * * * * * * * * * //
+
+template <class fieldType, class nodeType> 
+Foam::word 
+Foam::moment<fieldType, nodeType>::listToWord(const labelList& lst)
+{
+    word w;
+
+    forAll(lst, dimI)
+    {
+        w += Foam::name(lst[dimI]);
+    }
+
+    return w;
+}
+
+template <class fieldType, class nodeType> 
+Foam::label 
+Foam::moment<fieldType, nodeType>::listToLabel(const labelList& lst)
+{
+    label l = 0;
+
+    forAll(lst, dimI)
+    {
+        l += lst[dimI]*pow(10, lst.size() - dimI - 1);
+    }
+
+    return l;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template <class fieldType, class nodeType> 
+Foam::moment<fieldType, nodeType>::moment
+(
+    const labelList& cmptOrders,
+    const PtrList<nodeType>& nodes
+)
+:
+    fieldType
+    (
+        IOobject
+        (
+            IOobject::groupName("moment", listToWord(cmptOrders)), 
+            nodes[0].primaryWeight().mesh().time().timeName(),
+            nodes[0].primaryWeight().mesh(),
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        nodes[0].primaryWeight().mesh()
+    ),
+    nodes_(nodes),
+    cmptOrders_(cmptOrders),
+    name_(listToWord(cmptOrders_)),
+    nDimensions_(cmptOrders_.size()),
+    order_(sum(cmptOrders_))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+template <class fieldType, class nodeType> 
+Foam::moment<fieldType, nodeType>::~moment()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+template <class fieldType, class nodeType> 
+Foam::autoPtr<Foam::moment<fieldType, nodeType> > 
+Foam::moment<fieldType, nodeType>::clone() const
+{
+    notImplemented("moment::clone() const");
+    return autoPtr<moment<fieldType, nodeType> >(NULL);
+}
+
+template <class fieldType, class nodeType> 
+void Foam::moment<fieldType, nodeType>::update()
+{  
+    // Resetting the moment to zero
+    *this == dimensionedScalar("moment", (*this).dimensions(), 0);
+    
+    bool extendedNode = nodes_[0].extended();
+    
+    // If nodes do not have extended status, only use primary quadrature.
+    if (!extendedNode)
+    {
+        forAll(nodes_, pNodeI)
+        {
+            const nodeType& node = nodes_[pNodeI];
+            
+            if (!node.extended())
+            {
+                fieldType m = node.primaryWeight();
+            
+                for (label cmpt = 0; cmpt < nDimensions_; cmpt++)
+                {
+                    const label cmptMomentOrder = cmptOrders()[cmpt];
+                    
+                    tmp<fieldType> abscissaCmpt 
+                            = node.primaryAbscissa().component(cmpt);
+
+                    tmp<fieldType> mPow = m*pow(abscissaCmpt, cmptMomentOrder);
+                    m.dimensions().reset(mPow().dimensions());
+
+                    m == mPow;
+                }
+
+                *this == *this + m;
+            }
+        }
+
+        return;
+    }
+
+    // Extended quadrature case    
+    forAll(nodes_, pNodeI)
+    {
+        const nodeType& node = nodes_[pNodeI];
+
+        const fieldType& pW = node.primaryWeight();
+
+        for (label sNodeI = 0; sNodeI < node.nSecondaryNodes(); sNodeI++)
+        {     
+            fieldType m(pW*node.secondaryWeights()[sNodeI]);
+
+            for (label cmpt = 0; cmpt < nDimensions_; cmpt++)
+            {
+                const label cmptMomentOrder = cmptOrders()[cmpt];
+
+                tmp<fieldType> abscissaCmpt 
+                        = node.secondaryAbscissae()[sNodeI].component(cmpt);
+
+                tmp<fieldType> mPow = m*pow(abscissaCmpt, cmptMomentOrder);
+
+                m.dimensions().reset(mPow().dimensions());
+
+                m == mPow;
+            }
+
+            *this == *this + m;
+        }
+    }
+}
+
+
+// ************************************************************************* //
