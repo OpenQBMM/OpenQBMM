d42ae6ef4d069711e8a8d163cbe86ab5a0962a09
Alberto Passalacqua <apcfd@outlook.com>
2015-05-28 01:49:22 +0200

    DEV: Implemented templated class to store a list of moments and      map them for easy access through their indeces, as in      mathematical notation.
         Template arguments are the type of moment and of quadrature
         node.
    
         Base functionality is generic, and any moment dimensionality
         is supported. However, some access functions do are currently
         limited to moments with indeces represented by tuples of three
         elements. These functions are provided for convenience to
         operate on velocity moments, however no part of the class rely
         on them.

---
+++ src/quadratureMethods/moments/momentFieldSet.C
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2012 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+2015-03-10 Alberto Passalacqua: Templated class on the type of moment and of
+                                quadrature node.
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "momentFieldSet.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template <class momentType, class nodeType> 
+Foam::momentFieldSet<momentType, nodeType>::momentFieldSet
+(
+    const dictionary& dict,
+    const PtrList<nodeType>& nodes
+)
+:
+    PtrList<momentType>
+    (
+        dict.lookup("moments"), 
+        typename momentType::iNew(nodes)
+    ),
+    nodes_(nodes),
+    nDimensions_((*this)[0].nDimensions()), 
+    nMoments_((*this).size()), 
+    momentMap_(nMoments_)
+{
+    // Check on the number of moments and nodes may go here.
+    // However, to keep the implementation generic, it is moved to a 
+    // higher-level class where the specific quadrature method is implemented.
+
+    // Populate the moment set
+    forAll(*this, mI)
+    {
+        momentMap_.insert
+        (
+            moment<momentType, nodeType>::listToLabel
+            (
+                this->operator[](mI).cmptOrders()
+            ),
+            mI
+        );
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+template <class momentType, class nodeType> 
+Foam::momentFieldSet<momentType, nodeType>::~momentFieldSet()
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+template <class momentType, class nodeType> 
+void Foam::momentFieldSet<momentType, nodeType>::update()
+{
+    forAll(*this, mI)
+    {
+        this->operator[](mI).update();
+    }
+}
+
+
+// ************************************************************************* //
