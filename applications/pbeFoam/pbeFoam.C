9e20189f138791b9cf6dc8e041349a03817d3f2c
Alberto Passalacqua <apcfd@outlook.com>
2015-06-22 00:43:06 +0200

    DEV: Implemented pbeFoam, a zero-dimensional solver for verification      and validation studies of the population balance module.
         The implementation is based on chemFoam for the creation of a
         single-cell mesh. However, due to the data structure of
         populationBalance, the initial condition is provided in the
         traditional way used by OpenFOAM, rather than being generated
         by the code.

---
+++ applications/pbeFoam/pbeFoam.C
@@ -0,0 +1,85 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+2015-06-21 Alberto Passalacqua: Derived solver from chemFoam.
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    pbeFoam
+
+Description
+    Solver for population balance problems
+    - designed for use on single cell cases to provide comparison against
+      other chemistry solvers
+    - single cell mesh created on-the-fly
+    - fields created on the fly from the initial conditions
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "psiThermo.H"
+#include "turbulentFluidThermoModel.H"
+#include "OFstream.H"
+#include "thermoPhysicsTypes.H"
+#include "basicMultiComponentMixture.H"
+#include "cellModeller.H"
+#include "populationBalanceModel.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::noParallel();
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createSingleCellMesh.H"
+    #include "createFields.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+        #include "readControls.H"
+
+        #include "setDeltaT.H"
+
+        runTime++;
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        populationBalance->solve();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info << "Number of steps = " << runTime.timeIndex() << endl;
+    Info << "End" << nl << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
