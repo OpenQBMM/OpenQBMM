41fdcf545170c56fbce19be6b486244a68df16e1
Alberto Passalacqua <apcfd@outlook.com>
2015-07-29 10:36:24 -0500

    DEV: Removed fatal error in case of root not bracketed, and use      minimization.

--- src/quadratureMethods/extentedMomentInversion/extendedMomentInversion/extendedMomentInversion.C
+++ src/quadratureMethods/extentedMomentInversion/extendedMomentInversion/extendedMomentInversion.C
@@ -133,14 +133,20 @@ void Foam::extendedMomentInversion::invert(const univariateMomentSet& moments)
 
         if (fLow*fHigh > 0)
         {
-            FatalErrorIn
-            (
-                "Foam::extendedMomentInversion::invert\n"
-                "(\n"
-                "   const univariateMomentSet& moments\n"
-                ")"
-            )   << "Root not bracketed."
-                << abort(FatalError);
+//             FatalErrorIn
+//             (
+//                 "Foam::extendedMomentInversion::invert\n"
+//                 "(\n"
+//                 "   const univariateMomentSet& moments\n"
+//                 ")"
+//             )   << "Root not bracketed."
+//                 << abort(FatalError);
+
+            // Root not found. Minimize target function in [0, sigma_]
+            sigma_ = minimizeTargetFunction(0, sigmaHigh, m, mStar);
+            targetFunction(sigma_, m, mStar);
+            
+            // TODO Check realizability here
 
             return;
         }
