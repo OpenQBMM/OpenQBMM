5bfd36bc6ab9caa969def474a23efc8e10eb2455
Matteo Icardi <mat.icardi@gmail.com>
2016-02-24 15:41:18 +0000

    fullFragmentation

---
+++ src/quadratureMethods/populationBalanceModels/populationBalanceSubModels/daugtherDistributions/fullFragmentation/fullFragmentation.C
@@ -0,0 +1,82 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2015 Alberto Passalacqua
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fullFragmentation.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace populationBalanceSubModels
+{
+namespace daughterDistributions
+{
+    defineTypeNameAndDebug(fullFragmentation, 0);
+
+    addToRunTimeSelectionTable
+    (
+        daughterDistribution,
+        fullFragmentation,
+        dictionary
+    );
+}
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::daughterDistributions::fullFragmentation
+::fullFragmentation
+(
+    const dictionary& dict
+)
+:
+    daughterDistribution(dict)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::daughterDistributions::fullFragmentation
+::~fullFragmentation()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField>
+Foam::populationBalanceSubModels::daughterDistributions::fullFragmentation
+::mD
+(
+    const label order,
+    const volScalarField& abscissa
+) const
+{    
+    return pow(2.0, (3.0 - order)/3.0)*pow(abscissa, order);
+}
+
+// ************************************************************************* //
