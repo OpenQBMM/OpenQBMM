a7afd57266266dc1f3279571e6e24154c73f1009
Alberto Passalacqua <apcfd@outlook.com>
2017-08-01 17:37:22 -0500

    BUG: Corrected header check and linking library.

--- applications/solvers/polydisperseBubbleFoam/twoPhaseSystem/phaseModels/phaseModel/phaseModel.C
+++ applications/solvers/polydisperseBubbleFoam/twoPhaseSystem/phaseModels/phaseModel/phaseModel.C
@@ -153,7 +153,7 @@ Foam::phaseModel::phaseModel
         IOobject::NO_READ
     );
 
-    if (phiHeader.headerOk())
+    if (typeHeaderOk<surfaceScalarField>(true))
     {
         Info<< "Reading face flux field " << phiName << endl;
 
