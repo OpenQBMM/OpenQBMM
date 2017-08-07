b715e523fbb3bbddd211ed80daa0d4e7de4063c7
jheylmun <jheylmun@iastate.edu>
2017-08-07 15:36:03 -0500

    BUG: Corrected stress used in U1Eqn to use gas phase in place of particle

--- applications/solvers/multiphase/phaseCompressibleTurbulenceModels/kineticTheory/kineticTheoryModels/anisotropicGaussian/AGmomentTransportModel/AGmomentTransportModel.C
+++ applications/solvers/multiphase/phaseCompressibleTurbulenceModels/kineticTheory/kineticTheoryModels/anisotropicGaussian/AGmomentTransportModel/AGmomentTransportModel.C
@@ -289,8 +289,9 @@ void Foam::AGmomentTransportModel::solve
         m0 = m0Old - fvc::surfaceIntegrate(F0_)*deltaT;
         m0.correctBoundaryConditions();
 
-        volScalarField taup =
-            phase_.fluid().drag(phase_).Ki(0,0)/phase_.rho()*deltaT;
+        volScalarField taup = pos0(0.01 - alphap_)
+           *phase_.fluid().drag(phase_).Ki(0,0)/phase_.rho()*deltaT;
+
         const volVectorField& Uc = phase_.fluid().otherPhase(phase_).U();
 
         m1 = (m1Old - fvc::surfaceIntegrate(F1_)*deltaT + taup*alphap_*Uc)
