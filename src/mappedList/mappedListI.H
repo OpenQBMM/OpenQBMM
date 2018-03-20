9e88956b96fc66df541263042a0ca3ce85ed3a89
jheylmun <jheylmun@iastate.edu>
2018-03-20 12:21:31 -0500

    DEV: Improved generateMoments utility

--- src/mappedList/mappedListI.H
+++ src/mappedList/mappedListI.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2015-2017 Alberto Passalacqua
+    \\  /    A nd           | Copyright (C) 2015-2018 Alberto Passalacqua
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
