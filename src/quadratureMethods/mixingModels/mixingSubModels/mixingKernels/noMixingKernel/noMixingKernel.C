4ade42d91645765103c2d99b0e1e72578977a64d
Alberto Passalacqua <apcfd@outlook.com>
2018-06-28 23:53:06 +0200

    DEV: Implemented empty mixing kernel.

---
+++ src/quadratureMethods/mixingModels/mixingSubModels/mixingKernels/noMixingKernel/noMixingKernel.C
@@ -0,0 +1,91 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016-2018 Alberto Passalacqua
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "noMixingKernel.H"
+#include "addToRunTimeSelectionTable.H"
+#include "turbulentFluidThermoModel.H"
+#include "fundamentalConstants.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace mixingSubModels
+{
+namespace mixingKernels
+{
+    defineTypeNameAndDebug(noMixingKernel, 0);
+
+    addToRunTimeSelectionTable
+    (
+        mixingKernel,
+        noMixingKernel,
+        dictionary
+    );
+}
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::mixingSubModels::mixingKernels::noMixingKernel::noMixingKernel
+(
+    const dictionary& dict
+)
+:
+    mixingKernel(dict)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::mixingSubModels::mixingKernels::noMixingKernel
+::~noMixingKernel()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::fvScalarMatrix>
+Foam::mixingSubModels::mixingKernels::noMixingKernel::K
+(
+    const volUnivariateMoment& moment,
+    const volUnivariateMomentFieldSet& moments
+) const
+{
+    tmp<fvScalarMatrix> mixingK
+    (
+        new fvScalarMatrix
+        (
+            moment,
+            moment.dimensions()*dimVol/dimTime
+        )
+    );
+
+    return mixingK;
+}
+
+// ************************************************************************* //
