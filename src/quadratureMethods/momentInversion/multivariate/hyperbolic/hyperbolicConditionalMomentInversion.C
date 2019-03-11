7e63dca92961cae82617dd488f1fbcdf37639d1f
jheylmun <jheylmun@iastate.edu>
2019-03-11 15:56:27 -0500

    DEV: Added 3D jet crossing case with size-velocity distribution

--- src/quadratureMethods/momentInversion/multivariate/hyperbolic/hyperbolicConditionalMomentInversion.C
+++ src/quadratureMethods/momentInversion/multivariate/hyperbolic/hyperbolicConditionalMomentInversion.C
@@ -102,7 +102,7 @@ Foam::multivariateMomentInversions::CHyQMOM::CHyQMOM::threeDimNodeIndexes =
     {0, 0, 1},
     {0, 0, 2},
     {0, 1, 0},
-    {0, 1, 2},
+    {0, 1, 1},
     {0, 1, 2},
     {0, 2, 0},
     {0, 2, 1},
