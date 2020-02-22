9f1736beaf0fa0e0ca38c56b54563ca452513f72
Alberto Passalacqua <apcfd@outlook.com>
2020-02-21 23:53:23 -0600

    PRJ: Updated header.

--- src/quadratureMethods/hermiteQuadrature/hermiteQuadrature.C
+++ src/quadratureMethods/hermiteQuadrature/hermiteQuadrature.C
@@ -8,7 +8,7 @@
     Code created 2010-2014 by Bo Kong and 2015-2018 by Alberto Passalacqua
     Contributed 2018-07-31 to the OpenFOAM Foundation
     Copyright (C) 2018 OpenFOAM Foundation
-    Copyright (C) 2019 Alberto Passalacqua
+    Copyright (C) 2019-2020 Alberto Passalacqua
 -------------------------------------------------------------------------------
 License
     This file is derivative work of OpenFOAM.
