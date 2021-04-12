2529612146f0c4384f2d9b2f1a431fd7ccfcebc6
Alberto Passalacqua <apcfd@outlook.com>
2021-04-12 02:21:17 -0500

    ENH: Improved comments and introduced more descriptive variable names.

--- applications/utilities/computeMoments/computeMoments.C
+++ applications/utilities/computeMoments/computeMoments.C
@@ -76,6 +76,7 @@ int main(int argc, char *argv[])
     );
 
     wordList phaseNames(dict.lookup("phases"));
+
     forAll(phaseNames, phasei)
     {
         velocityQuadratureApproximation quadrature
@@ -84,37 +85,44 @@ int main(int argc, char *argv[])
             mesh,
             "RPlus"
         );
+
         autoPtr<mappedPtrList<volVelocityNode>> nodes(&(quadrature.nodes()));
 
-        PtrList<dimensionSet> abscissaeDims(quadrature.momentOrders()[0].size());
+        PtrList<dimensionSet> abscissaeDimensions
+        (
+            quadrature.momentOrders()[0].size()
+        );
+
         labelList scalarIndexes = nodes()[0].scalarIndexes();
+
         if (scalarIndexes.size() == 0)
         {
             scalarIndexes.append(-1);
         }
+
         label si = 0;
 
         forAll(quadrature.momentOrders()[0], cmpti)
         {
             if (cmpti == scalarIndexes[si])
             {
-                abscissaeDims.set
+                abscissaeDimensions.set
                 (
                     cmpti,
                     new dimensionSet(nodes()[0].primaryAbscissae()[si].dimensions())
                 );
+
                 si++;
             }
             else
             {
-                abscissaeDims.set
+                abscissaeDimensions.set
                 (
                     cmpti,
                     new dimensionSet(dimVelocity)
                 );
             }
         }
-
 
         labelListList newMomentOrders
         (
@@ -124,9 +132,10 @@ int main(int argc, char *argv[])
         forAll(newMomentOrders, mi)
         {
             dimensionSet mDims(nodes()[0].primaryWeight().dimensions());
-            forAll(abscissaeDims, cmpti)
+
+            forAll(abscissaeDimensions, cmpti)
             {
-                mDims *= pow(abscissaeDims[cmpti], newMomentOrders[mi][cmpti]);
+                mDims *= pow(abscissaeDimensions[cmpti], newMomentOrders[mi][cmpti]);
             }
 
             volVelocityMoment moment
@@ -147,14 +156,14 @@ int main(int argc, char *argv[])
                     quadrature.moments()[0].boundaryField().types()
                 )
             );
+
             Info<< "Created " << moment.name() << endl;
+
             moment.update();
             moment.updateBoundaries();
             moment.write();
         }
     }
-
-
 
     Info<< nl << "End\n" << endl;
 
