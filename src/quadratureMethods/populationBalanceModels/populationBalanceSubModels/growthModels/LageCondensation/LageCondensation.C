28da79a167b5c064e3e23b552b5265a9c174901f
Alberto Passalacqua <apcfd@outlook.com>
2022-02-13 20:35:38 -0600

    DEV: Implemented diffusion-driven growth model of Pruppacher and      Klett (1980) and condensation model of Lage (2011).
         Added test cases from these models with GQMOM:
    
         - Pure condensation on R+ (McGraw, 1997)
         - Pure condensation on finite-size domain (Strumendo and
           Arastoopour, 2008).

---
+++ src/quadratureMethods/populationBalanceModels/populationBalanceSubModels/growthModels/LageCondensation/LageCondensation.C
@@ -0,0 +1,85 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | OpenQBMM - www.openqbmm.org
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2021-2022 Alberto Passalacqua
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "LageCondensation.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace populationBalanceSubModels
+{
+namespace growthModels
+{
+    defineTypeNameAndDebug(LageCondensation, 0);
+
+    addToRunTimeSelectionTable
+    (
+        growthModel,
+        LageCondensation,
+        dictionary
+    );
+}
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::growthModels::LageCondensation
+::LageCondensation
+(
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    growthModel(dict, mesh)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::growthModels::LageCondensation
+::~LageCondensation()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::scalar
+Foam::populationBalanceSubModels::growthModels::LageCondensation::Kg
+(
+    const scalar& abscissa,
+    const bool lengthBased,
+    const label environment
+) const
+{
+    return 0.5*abscissa;
+}
+
+// ************************************************************************* //
