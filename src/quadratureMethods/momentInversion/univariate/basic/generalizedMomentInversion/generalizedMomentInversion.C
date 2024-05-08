ea36f7d79d3d50a64dceb29d8108a4118838acb6
Alberto Passalacqua <apcfd@outlook.com>
2024-05-08 17:59:00 -0500

    DEV: Converted integration of turbulent mixing models      to use the realizable ODE solver.

--- src/quadratureMethods/momentInversion/univariate/basic/generalizedMomentInversion/generalizedMomentInversion.C
+++ src/quadratureMethods/momentInversion/univariate/basic/generalizedMomentInversion/generalizedMomentInversion.C
@@ -5,7 +5,7 @@
     \\  /    A nd           | OpenQBMM - www.openqbmm.org
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2021-2023 Alberto Passalacqua
+    Copyright (C) 2021-2024 Alberto Passalacqua
 -------------------------------------------------------------------------------
 License
     This file is derivative work of OpenFOAM.
@@ -204,7 +204,6 @@ void Foam::generalizedMomentInversion::correctRecurrenceRPlus
         return; // Use Gauss if no additional nodes are possible 
     }
 
-    label nMoments = moments.size();
     moments.zetas().resize(2*nMaxNodes_ - 1, 0.0);
 
     if (ndfTypeRPlus_ == "gamma")
@@ -223,8 +222,9 @@ void Foam::generalizedMomentInversion::correctRecurrenceRPlus
                 (i + alphaCoeff)*moments.zetas()[2*nRegularQuadratureNodes_ - 3]
                /(nRegularQuadratureNodes_ - 1 + alphaCoeff);
 
-            moments.zetas()[2*i] = (i + 1)*moments.zetas()[2*nRegularQuadratureNodes_ - 2]
-                        /(nRegularQuadratureNodes_);
+            moments.zetas()[2*i] = 
+                (i + 1)*moments.zetas()[2*nRegularQuadratureNodes_ - 2]
+               /(nRegularQuadratureNodes_);
 
             // Debug info
             //Info << "zetas[2*i-1] = " << zetas[2*i - 1] << endl;
@@ -244,13 +244,17 @@ void Foam::generalizedMomentInversion::correctRecurrenceRPlus
             i++
         )
         {
-            moments.zetas()[2*i - 1] = pow(eta, 2*(i + 1 - nRegularQuadratureNodes_))
-                        *((pow(eta, 2*(i+1)) - 1.0)
-                            /(pow(eta, 2*nRegularQuadratureNodes_) - 1.0))
-                        *moments.zetas()[2*nRegularQuadratureNodes_ - 3];
+            moments.zetas()[2*i - 1] = 
+                pow(eta, 2*(i + 1 - nRegularQuadratureNodes_))
+               *(
+                    (pow(eta, 2*(i+1)) - 1.0)
+                   /(pow(eta, 2*nRegularQuadratureNodes_) - 1.0)
+                )
+               *moments.zetas()[2*nRegularQuadratureNodes_ - 3];
 
-            moments.zetas()[2*i] = pow(eta, 4*(i + 1 - nRegularQuadratureNodes_))
-                        *moments.zetas()[2*nRegularQuadratureNodes_ - 2];
+            moments.zetas()[2*i] = 
+                pow(eta, 4*(i + 1 - nRegularQuadratureNodes_))
+               *moments.zetas()[2*nRegularQuadratureNodes_ - 2];
             // Info << "zetas[2*i-1] = " << moments.zetas()[2*i - 1] << endl;
             // Info << "2i-1 = " << 2*i - 1 << endl;
             // Info << "zetas[2*i] = " << moments.zetas()[2*i] << endl;
@@ -319,32 +323,37 @@ void Foam::generalizedMomentInversion::correctRecurrence01
          || pJ2n_1 >= pJ2i_1)
         {
             moments.canonicalMoments()[2*i - 1] = 
-                moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 3]*pJ2i_1/pJ2n_1;
+                moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 3]
+               *pJ2i_1/pJ2n_1;
         }
         else
         {
             moments.canonicalMoments()[2*i - 1] = 
-                (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 3]*(1.0 - pJ2i_1) 
-                    + pJ2i_1 - pJ2n_1)/(1.0 - pJ2n_1);
+                (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 3]
+               *(1.0 - pJ2i_1) + pJ2i_1 - pJ2n_1)/(1.0 - pJ2n_1);
         }
 
         if (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2] <= pJ2n
          || pJ2n >= pJ2i)
         {
             moments.canonicalMoments()[2*i] = 
-                moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2]*pJ2i/pJ2n;
+                moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2]
+               *pJ2i/pJ2n;
         }
         else
         {
             moments.canonicalMoments()[2*i] = 
-                (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2]*(1.0 - pJ2i) 
-                    + pJ2i - pJ2n)/(1.0 - pJ2n);
+                (moments.canonicalMoments()[2*nRegularQuadratureNodes_ - 2]
+               *(1.0 - pJ2i) + pJ2i - pJ2n)/(1.0 - pJ2n);
         }
 
-        moments.zetas()[2*i - 1] 
-            = moments.canonicalMoments()[2*i - 1]*(1.0 - moments.canonicalMoments()[2*i - 2]);
+        moments.zetas()[2*i - 1] = 
+            moments.canonicalMoments()[2*i - 1]
+           *(1.0 - moments.canonicalMoments()[2*i - 2]);
 
-        moments.zetas()[2*i] = moments.canonicalMoments()[2*i]*(1.0 - moments.canonicalMoments()[2*i - 1]);
+        moments.zetas()[2*i] = 
+            moments.canonicalMoments()[2*i]
+           *(1.0 - moments.canonicalMoments()[2*i - 1]);
     }
 
     alpha[0] = moments.zetas()[0];
